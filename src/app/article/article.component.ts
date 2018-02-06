3b36a9611da0772f8706149ea30a62e6e1a77a85
Aoife Egan <S00167887@mail.itsligo.ie>
2018-02-06 15:24:59 +0000

    new changes

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+//import { HostBinding } from '@angular/core/src/metadata/directives';
 
 @Component({
   selector: 'app-article',
@@ -6,8 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+  //want each article to be on its own row
+  //using SemanticUI-provides class for rows called 'row'
+  @HostBinding('attr.class') cssClass = 'row';
+  votes: number;
+  title: string;
+  link: string;
 
-  constructor() { }
+  constructor() {
+    this.title = 'Angular 2';
+    this.link = 'http://angular.io';
+    this.votes = 10;
+   }
+
+   voteUp() {
+     this.votes += 1;
+   }
+   voteDown(){
+     this.votes -= 1;
+   }
 
   ngOnInit() {
   }
