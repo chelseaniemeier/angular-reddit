f81b96f8fb90d4689ad826c41d21e77396cb137c
Aoife Egan <S00167887@mail.itsligo.ie>
2018-02-06 15:08:00 +0000

    changes

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  //title = 'app works!';
+  //method to add an article when the submit button is clicked on the html form
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    return false;
+  }
+}
